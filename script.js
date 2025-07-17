1e8e28e4fb9ddecf634f09788736202baefc9156
Erin Hollett <erinkhollett@gmail.com>
2025-07-17 18:54:46 -0400

    Fixed navigation and added JS form validation

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+function validateForm() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+
+  if (name === '' || email === '' || subject === '' || message === '') {
+    return false;
+  }
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.text(email)) {
+    return false;
+  }
+
+  return true;
+}
